5f0d7f34290c2de2e2dc14172480a9b0e220e408
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-02-25 11:50:48 -0300

    update

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 
-router.get("/posts", postController.getAllPosts);
-router.post("/posts", postController.addPost);
-router.get("/posts/:id", postController.getPostById);
-router.put("/posts/:id", postController.updatePost);
-router.delete("/posts/:id", postController.deletePost);
+router.get("/post", postController.getAllPosts);
+router.post("/post", postController.addPost);
+router.get("/post/:id", postController.getPostById);
+router.put("/post/:id", postController.updatePost);
+router.delete("/post/:id", postController.deletePost);
 
 module.exports = router;
